8233275ea11ff5c5ae372b54e7c96e80d3b4256e
Kagenooi <kagenooi.morpheus@gmail.com>
2025-02-09 15:41:55 +0400

    update adaptive mode

--- adaptiveMode/adaptive.js
+++ adaptiveMode/adaptive.js
@@ -1,17 +1,14 @@
 export function adaptive() {
     const wrapper = document.querySelector('#adaptive');
     let zoom = document.body.clientWidth / 19.2 / 100;
-    if (document.body.clientWidth > 1380 && document.body.clientWidth < 1440) {
-        zoom = document.body.clientWidth / 14.4 / 100;
-    }
-    if (document.body.clientWidth > 980 && document.body.clientWidth < 1280) {
+    if (document.body.clientWidth > 768 && document.body.clientWidth < 1440) {
         zoom = document.body.clientWidth / 12.8 / 100;
     }
-    if (document.body.clientWidth > 520 && document.body.clientWidth < 981) {
-        zoom = document.body.clientWidth / 7.68 / 100;
+    if (document.body.clientWidth > 480 && document.body.clientWidth < 768) {
+        zoom = document.body.clientWidth / 6.4 / 100;
     }
-    if (document.body.clientWidth < 521) {
-        zoom = document.body.clientWidth / 3.6 / 100;
+    if (document.body.clientWidth < 480) {
+        zoom = document.body.clientWidth / 3.2 / 100;
     }
     wrapper.style.zoom = zoom;
 
