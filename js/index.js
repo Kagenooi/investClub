981d91152717c8a5a386bce117dc694681f34487
Kagenooi <kagenooi.morpheus@gmail.com>
2025-02-10 16:37:12 +0400

    about and product desktop, import swiper for resident block

--- js/index.js
+++ js/index.js
@@ -5,7 +5,24 @@ window.addEventListener('resize', function () { adaptive() });
 
 
 import {
-    toggleActive
+    toggleActive,
+    tabs
 } from "./module.js";
 
 window.toggleActive = toggleActive;
+window.tabs = tabs;
+document.querySelector('#defaultProduct').click();
+
+
+const resident = new Swiper(".resident__slider", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".resident__slider_btn.next",
+        prevEl: ".resident__slider_btn.prev",
+    },
+    pagination: {
+        el: ".resident__slider_pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
--- js/module.js
+++ js/module.js
@@ -1,3 +1,17 @@
 export function toggleActive(elem) {
     document.querySelector(`#${elem}`).classList.toggle('active');
 }
+
+export function tabs(btn, tabs, btns) {
+    let wrapper = document.querySelectorAll(`.${tabs}`);
+    wrapper.forEach(element => {
+        element.classList.remove('active');
+    });
+    document.querySelector(`#${btn.dataset.tab}`).classList.add('active');
+
+    let control = document.querySelectorAll(`.${btns}`);
+    control.forEach(element => {
+        element.classList.remove('active');
+    });
+    btn.classList.add('active');
+}
