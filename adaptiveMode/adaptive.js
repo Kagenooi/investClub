c205a6d8e4020499c3f88cfd9944ea9874ca0716
Kagenooi <kagenooi.morpheus@gmail.com>
2025-03-18 20:43:04 +0400

    asd

--- adaptiveMode/adaptive.js
+++ adaptiveMode/adaptive.js
@@ -1,13 +1,13 @@
 export function adaptive() {
     const wrapper = document.querySelector('#adaptive');
     let zoom = document.body.clientWidth / 19.2 / 100;
-    if (document.body.clientWidth > 990 && document.body.clientWidth < 1460) {
+    if (document.body.clientWidth > 975 && document.body.clientWidth < 1406) {
         zoom = document.body.clientWidth / 12.8 / 100;
     }
-    if (document.body.clientWidth > 576 && document.body.clientWidth < 1000) {
+    if (document.body.clientWidth > 576 && document.body.clientWidth < 977) {
         zoom = document.body.clientWidth / 6.4 / 100;
     }
-    if (document.body.clientWidth < 600) {
+    if (document.body.clientWidth < 577) {
         zoom = document.body.clientWidth / 3.2 / 100;
     }
     wrapper.style.zoom = zoom;
